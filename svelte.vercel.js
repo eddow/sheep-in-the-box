320fa633c8c4b1c5e62e07c6321f6157f02eef1d
fmdm <eeddow@gmail.com>
2023-04-01 11:17:18 +0300

    config

--- svelte.vercel.js
+++ svelte.vercel.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { nodeLoaderPlugin } from "@vavite/node-loader/plugin";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,9 @@ const config = {
 	compilerOptions: {
 		enableSourcemap: true,
 	},
+	plugins: [
+		nodeLoaderPlugin()
+	],
 	kit: {
 		adapter: adapter(),
 		alias: {
