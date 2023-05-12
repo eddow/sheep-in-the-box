c2bdc5d4877eb72208dd348df1941ab89ef20799
François Marie De Mey <eeddow@gmail.com>
2023-05-12 13:39:19 +0300

    source-less

---
+++ src/entities/sitb/index.ts
@@ -0,0 +1,4 @@
+export * from './article';
+export * from './intl';
+export * from './raw';
+export * from './user';
--- src/sitb/server/config.ts
+++ src/sitb/server/config.ts
@@ -1,12 +1,11 @@
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 import { ReflectMetadataProvider } from "@mikro-orm/core";
 import type { Configuration, Connection, IDatabaseDriver, Options } from '@mikro-orm/core'
+import * as entities from '$entities';
 
 export default function config(env: Record<string, string|undefined>): Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> {
 	return {
-		debug: ['discovery'],
-		entities: ['./src/entities/**/*.ts'],
-		//entitiesTs: ['./src/entities/**/*.ts'],
+		entities: Object.values(entities),
 		metadataProvider: ReflectMetadataProvider,
 		// TODO: this last part should be project-dependant
 		type: "mongo",
